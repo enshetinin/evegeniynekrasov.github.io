2c25255a59e1a11c031e45b2f087d58842de5318
Evgeniy Nekrasov Shetinin <enshetinin@gmail.com>
2026-06-27 18:10:30 +0200

    remove vite, will use only html and css

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css';
-
-const app = document.getElementById("app") as HTMLDivElement | undefined;
-
-if (!app) throw new Error("root 'app' element is missing");
-
-app.textContent = "Staring point";
-
-
