291e8de7f05e0078d319a6b3bdcff9e9911aa5ec
Evgeniy Nekrasov Shetinin <enshetinin@gmail.com>
2026-07-19 13:00:45 +0200

    change some configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-const owner = process.env.GITHUB_REPOSITORY_OWNER;
-const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
-const isUserPage =
-    owner !== undefined &&
-    repository !== undefined &&
-    repository === `${owner}.github.io`;
-
 export default defineConfig({
-    site: owner ? `https://${owner}.github.io` : "http://localhost:4321",
-    base: owner && repository && !isUserPage ? `/${repository}` : "/",
-    trailingSlash: "always",
+    site: "https://enshetinin.github.io",
 });
